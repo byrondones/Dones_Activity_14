80317537b498bf8da86cd628f670dad1fac9fe87
itstrazu <donesbyron05@gmail.com>
2023-10-20 09:20:36 +0800

    added services route

---
+++ src/pages/Services.jsx
@@ -0,0 +1,14 @@
+function Services(){
+
+    return(
+        <>
+            <section>
+                <h1>This is a Services Page</h1>
+                <p>This is a example</p>
+            </section>
+        </>
+    )
+
+}
+
+export default Services

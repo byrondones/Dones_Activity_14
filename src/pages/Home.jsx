3297440bf902f565d28f3f90dbe3b69664b0366b
itstrazu <donesbyron05@gmail.com>
2023-10-20 09:20:32 +0800

    added home route

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+function Home(){
+
+    return(
+        <>
+            <section>
+                <h1>This is a Home Page</h1>
+                <p>This is a example</p>
+            </section>
+        </>
+    )
+
+}
+
+export default Home

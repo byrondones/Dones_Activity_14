0e7be48b180fbf7ee851d22d5fbe2b0a7dcbe0e8
itstrazu <donesbyron05@gmail.com>
2023-10-20 09:20:11 +0800

    added nav bar to layout with links

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -6,7 +6,7 @@ function Layout(){
         <>
             <nav className="navbar navbar-expand-lg bg-dark">
                 <div className="container-fluid">
-                    <Link className='navbar-brand fw-bold text-white' to='/'>React Routing Demo</Link>
+                    <Link className='navbar-brand fw-bold text-white' to='/'>Routes</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>

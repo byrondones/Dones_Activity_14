a17bc8ac8ec385832cc248ca0da0adf8644460a3
itstrazu <donesbyron05@gmail.com>
2023-10-20 09:17:52 +0800

    created and added style to notfound route

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+function NotFound(){
+
+    return(
+        <div className="container-fluid text-center bg-dark" style={{padding:438.5}}>
+            <h1 className='text-white fw-bold'>404 Not Found</h1>
+        </div>
+    )
+
+}
+
+export default NotFound

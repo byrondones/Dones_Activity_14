10e0d9b1bff0d49725ac5bc45e8affb5bcd9ea5c
itstrazu <donesbyron05@gmail.com>
2023-10-20 09:20:22 +0800

    added about route

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+function About(){
+
+    return(
+        <>
+            <section>
+                <h1>This is a About Page</h1>
+                <p>This is a example</p>
+            </section>
+        </>
+    )
+
+}
+
+export default About

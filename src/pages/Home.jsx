f734b29472c1f01940c50a66a131ff2250993744
itstrazu <donesbyron05@gmail.com>
2023-10-22 05:28:17 +0800

    added hero section to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,31 @@
+import React from "react"
+import './css/Styles.css'
+
 function Home(){
 
     return(
         <>
-            <section>
-                <h1>This is a Home Page</h1>
-                <p>This is a example</p>
-            </section>
+            {/* Add a hero image */}
+
+        <div className="container-fluid">
+            <div className="row hero-image">
+                <div className="col-12">
+                <section className="hero-section p-5 text-center">
+                        <p className='text-white fw-bold'
+                            style={{isolation:'isolate', fontSize:64, paddingTop:150}}
+                        >Adopt Now</p>
+
+                        <p className='text-white pt-3 pb-3'
+                            style={{isolation:'isolate', fontSize:32}}>
+                        Embracing a new chapter of love and family. 💖👨‍👩‍👧‍👦 #AdoptNow
+                        </p>
+
+                    <button className='btn btn-dark fw-bold btn-lg' style={{isolation:'isolate'}}>Book for an adoption</button>
+                </section>
+                </div>
+            </div>
+        </div>
+
         </>
     )
 

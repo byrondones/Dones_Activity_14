23047eebb7a9157d7d6059afffb1de9a943075aa
itstrazu <donesbyron05@gmail.com>
2023-10-22 05:30:51 +0800

    added items to services page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,10 +2,60 @@ function Services(){
 
     return(
         <>
-            <section>
-                <h1>This is a Services Page</h1>
-                <p>This is a example</p>
-            </section>
+        <div className="container">
+            <div className="row bg-dark">
+                <div className="col-12">
+                <section className="hero-section p-5">
+                        <p className='text-white fw-bold text-sm-center'
+                            style={{isolation:'isolate', fontSize:32}}
+                        >Services Offered</p>
+
+                        <ul className="list-group">
+                            <li className="list-group-item">
+                                <b>Pet Health Clinics: </b>
+                                Offer regular health check-ups, vaccinations, and preventive care services for pets to ensure they lead healthy lives.
+                            </li>
+                            <li className="list-group-item">
+                                <b>Behavioral Training Classes: </b>
+                                Provide training classes to address behavioral issues in pets, helping owners better understand and communicate with their animals.
+                            </li>
+                            <li className="list-group-item">
+                                <b>Pet Grooming and Spa Services: </b>
+                                Offer grooming services such as baths, haircuts, and nail trims, as well as spa treatments for pets to keep them looking and feeling their best.
+                            </li>
+                            <li className="list-group-item">
+                                <b>Boarding and Daycare Facilities: </b>
+                                Offer regular health check-ups, vaccinations, and preventive care services for pets to ensure they lead healthy lives.
+                            </li>
+                            <li className="list-group-item">
+                                <b>Pet Food and Supplies Store: </b>
+                                Stock pet food, accessories, toys, and other pet-related products to provide a one-stop shop for pet owners.
+                            </li>
+                            <li className="list-group-item">
+                                <b>Emergency Veterinary Care: </b>
+                                If feasible, offer emergency veterinary services or collaborate with a nearby emergency clinic for after-hours care.
+                            </li>
+                            <li className="list-group-item">
+                                <b>Pet-Friendly Events and Workshops: </b>
+                                Organize events and workshops on topics like pet health, nutrition, and behavior. This can help build a sense of community among pet 
+                            </li>
+                            <li className="list-group-item">
+                                <b>Pet Adoption Fairs: </b>
+                                Host regular adoption fairs to connect more animals with potential adopters.
+                            </li>
+                            <li className="list-group-item">
+                                <b>Foster Care Program: </b>
+                                Develop a foster care program for pets awaiting adoption, giving them temporary homes and socialization.
+                            </li>
+                            <li className="list-group-item">
+                                <b>Community Outreach: </b>
+                                Collaborate with schools, local organizations, and businesses to promote responsible pet ownership and pet adoption.
+                            </li>
+                        </ul>
+                </section>
+                </div>
+            </div>
+        </div>
         </>
     )
 

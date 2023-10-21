4f6ab8a7ae6812c5e088444b5a006e1e8a4c8c3c
itstrazu <donesbyron05@gmail.com>
2023-10-22 05:29:42 +0800

    added details to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,10 +2,23 @@ function About(){
 
     return(
         <>
-            <section>
-                <h1>This is a About Page</h1>
-                <p>This is a example</p>
-            </section>
+        <div className="container-fluid">
+            
+            <div className="row bg-dark">
+                <div className="col-12">
+                <section className="hero-section p-5">
+                        <p className='text-light fw-bold text-sm-center'
+                            style={{isolation:'isolate', fontSize:32}}
+                        >About Us</p>
+
+                        <p className='text-light pt-3 pb-3 lh-lg'
+                            style={{isolation:'isolate', fontSize:16}}>
+                        Pet Haven Adoption Center is more than just a place; it's a haven for animals seeking love and care. We are driven by a deep commitment to the welfare of these wonderful creatures. Our core mission is to be the bridge that connects these deserving pets with compassionate individuals and families, forging lifelong bonds and nurturing happy homes. We firmly believe in second chances, and our dedicated team is here to make that belief a reality. We work tirelessly to rescue, rehabilitate, and rehome animals, ensuring they have the chance to thrive in an environment filled with love, care, and understanding. Each adoption from Pet Haven is a success story, a testament to the power of compassion and the transformative impact of a loving home. Together, we are making a positive change in the lives of both animals and their human companions.
+                        </p>
+                </section>
+                </div>
+            </div>
+        </div>
         </>
     )
 
